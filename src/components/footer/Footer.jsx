525eec6fe8dbafd374ea123e7d902ad12cbcac88
Muhsin TT <muhsintt777@gmail.com>
2022-11-26 06:31:12 +0530

    added text and css to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <h4>copyright@restocafe</h4>
+      <p>
+        aiuehfi iaufhijkdvi rioahksd frioafaoi oiejfo ojefW jepfihe ewoif
+        oiffoofv naisufha apfoiaoijsdjc eFJPEIF EJFKJSVN Oiejee;fjeoifj soij
+        eoijfldj iefso ids:cdsd foejvn
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
